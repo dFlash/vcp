48199cb233590fc547ac68cb481628e14c4efa7a
DMaliavin <dima_91@ukr.net>
2016-04-30 14:57:00 +0300

    Fix. Test data was removed

--- src/main/java/com/maliavin/vcp/controller/UserController.java
+++ src/main/java/com/maliavin/vcp/controller/UserController.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maliavin.vcp.domain.User;
 import com.maliavin.vcp.domain.Video;
 import com.maliavin.vcp.form.UploadForm;
-import com.maliavin.vcp.form.UploadForm2;
 import com.maliavin.vcp.security.SecurityUtils;
 import com.maliavin.vcp.service.UserService;
 
